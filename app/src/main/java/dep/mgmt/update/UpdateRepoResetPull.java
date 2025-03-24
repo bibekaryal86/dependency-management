e599d889ed9ff8d05389c688b5f9759288fc54ca
Bibek Aryal <bibekaryal86@gmail.com>
2025-03-23 18:18:35 -0600

    Reset Pull

---
+++ app/src/main/java/dep/mgmt/update/UpdateRepoResetPull.java
@@ -0,0 +1,40 @@
+package dep.mgmt.update;
+
+import dep.mgmt.model.AppData;
+import dep.mgmt.model.AppDataScriptFile;
+import dep.mgmt.util.ConstantUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class UpdateRepoResetPull {
+  private static final Logger log = LoggerFactory.getLogger(UpdateRepoResetPull.class);
+
+  private final String repoHome;
+  private final AppDataScriptFile scriptFile;
+  private final boolean isPull;
+  private final boolean isReset;
+
+  public UpdateRepoResetPull(final AppData appData, final boolean isPull, final boolean isReset) {
+    this.repoHome = appData.getArgsMap().get(ConstantUtils.ENV_REPO_HOME);
+    this.scriptFile =
+        appData.getScriptFiles().stream()
+            .filter(sf -> sf.getScriptName().equals(ConstantUtils.SCRIPT_RESET_PULL))
+            .findFirst()
+            .orElseThrow(
+                () -> new IllegalStateException("Github Pull Reset Script Not Found..."));
+    this.isPull = isPull;
+    this.isReset = isReset;
+  }
+
+  public void execute() {
+    log.debug("Execute Github Pull Reset on: [ {} ]", this.repoHome);
+    List<String> arguments = new LinkedList<>();
+    arguments.add(this.repoHome);
+    arguments.add(String.valueOf(this.isReset));
+    arguments.add(String.valueOf(this.isPull));
+    new ExecuteScriptFile(this.scriptFile.getScriptFileName(), arguments, Boolean.TRUE);
+  }
+}
