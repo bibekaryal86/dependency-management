da61259a451e5701fb503091a24a3b723435917e
Bibek Aryal <bibekaryal86@gmail.com>
2025-10-08 23:10:17 -0600

    MongoDb Fix

--- app/src/main/java/dep/mgmt/config/MongoDbConfig.java
+++ app/src/main/java/dep/mgmt/config/MongoDbConfig.java
@@ -20,19 +20,18 @@ public class MongoDbConfig {
     final String dbUser = CommonUtilities.getSystemEnvProperty(ConstantUtils.ENV_DB_USER);
     final String dbPwd = CommonUtilities.getSystemEnvProperty(ConstantUtils.ENV_DB_PWD);
     final String connectionString =
-        String.format(dbHost, dbUser, dbPwd, dbName, dbName.toUpperCase());
+            String.format(dbHost, dbUser, dbPwd, dbName, dbName.toUpperCase());
 
     CodecRegistry pojoCodecRegistry =
-        CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build());
+            CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build());
     CodecRegistry codecRegistry =
-        CodecRegistries.fromRegistries(
-            MongoClientSettings.getDefaultCodecRegistry(), pojoCodecRegistry);
-    try (MongoClient mongoClient = MongoClients.create(connectionString)) {
-      database =
-          mongoClient
-              .getDatabase(ConstantUtils.MONGODB_DATABASE_NAME)
-              .withCodecRegistry(codecRegistry);
-    }
+            CodecRegistries.fromRegistries(
+                    MongoClientSettings.getDefaultCodecRegistry(), pojoCodecRegistry);
+    MongoClient mongoClient = MongoClients.create(connectionString);
+    database =
+            mongoClient
+                    .getDatabase(ConstantUtils.MONGODB_DATABASE_NAME)
+                    .withCodecRegistry(codecRegistry);
   }
 
   public static MongoDatabase getDatabase() {
