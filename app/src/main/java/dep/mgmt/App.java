2727cb6619c0326ad3616669d66a9089663a0f73
Bibek Aryal <bibekaryal86@gmail.com>
2026-03-15 09:34:21 -0600

    Mongo DB Refactor, contd

--- app/src/main/java/dep/mgmt/App.java
+++ app/src/main/java/dep/mgmt/App.java
@@ -17,7 +17,7 @@ import org.slf4j.LoggerFactory;
 public class App {
   private static final Logger log = LoggerFactory.getLogger(App.class);
 
-  public static void main(String[] args) throws Exception {
+  static void main(String[] args) throws Exception {
     log.info("Starting Dependency Management Service...");
     App.init();
     ScheduleConfig.init();
